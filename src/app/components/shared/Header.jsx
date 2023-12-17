4fba2f6403e08a8a13e2b282e6f0f807be424e6c
Dharmvir Dharmacharya <ddharmacharya22@gmail.com>
2023-12-17 14:27:29 +0530

    header fix

--- src/app/components/shared/Header.jsx
+++ src/app/components/shared/Header.jsx
@@ -72,10 +72,7 @@ const Header = () => {
               </svg>
             )}
           </button>
-          <div
-            className={classNames("w-full md:block md:w-auto", mobileMenu)}
-            id="navbar-default"
-          >
+          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
                 <a
@@ -139,7 +136,10 @@ const Header = () => {
           </div>
         </div>
       </nav>
-      <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div
+        className={classNames("w-full lg:hidden md:w-auto", mobileMenu)}
+        id="navbar-default"
+      >
         <ul className="font-medium flex flex-col items-center p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
           <li>
             <a
