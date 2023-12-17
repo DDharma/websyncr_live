38e86fbb7fff2385ff9cecf67daf77b794409e01
Dharmvir Dharmacharya <ddharmacharya22@gmail.com>
2023-12-17 14:11:38 +0530

    google analytics tag added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,19 +1,33 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Websyncr',
-  description: 'Elevate Your Online Presence in 24-Hour',
-}
+  title: "Websyncr",
+  description: "Elevate Your Online Presence in 24-Hour",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-L3XLMM6PY7"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-L3XLMM6PY7')`,
+        }}
+      />
+
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
