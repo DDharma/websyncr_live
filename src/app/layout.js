85330261871e9922dcd413c278772760d0efc593
Dharmvir Dharmacharya <ddharmacharya22@gmail.com>
2024-03-28 17:07:21 +0530

    aaded analytics tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,21 @@ export default function RootLayout({ children }) {
       /> */}
       <script
         dangerouslySetInnerHTML={{
+          __html: `async src="https://www.googletagmanager.com/gtag/js?id=G-E5QYGTFSZ6"
+        `,
+        }}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-E5QYGTFSZ6')`,
+        }}
+      />
+      <script
+        dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
